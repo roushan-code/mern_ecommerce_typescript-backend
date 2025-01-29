c0cc3f22cbf81164741457a9bfb85cb301e503ef
roushan-code <roushansingh3698@gmail.com>
2025-01-29 16:07:03 +0530

    url

--- dist/index.js
+++ dist/index.js
@@ -18,7 +18,7 @@ config({
 });
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     credentials: true,
 }));
 app.use(express.json());
